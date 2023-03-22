3c07c95ebe72942f989159667200ce9abe62209e
Plorthos <tlkb@protonmail.com>
2023-03-22 19:37:28 -0400

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,6 +2,7 @@ module.exports = {
     ci: {
       collect: {
         staticDistDir: './',
+        staticDistDir: './blog2/public',
       },
       upload: {
         target: 'temporary-public-storage',
